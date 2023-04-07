649c59674519306aa48d1e5e703ac920fdf6cc68
Reagan615 <419247451@qq.com>
2023-04-07 15:22:09 -0500

    create

---
+++ Advanced-Topics-in-C-Sharp_Lab05/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Advanced_Topics_in_C_Sharp_Lab05.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
